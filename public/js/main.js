18456e94f1c9b91360a7c0620493f6e10dbea812
unknown <nworjiifeanyi@gmail.com>
2019-09-20 09:27:55 +0100

    added edit/update functionality and  delete functionality

--- public/js/main.js
+++ public/js/main.js
@@ -32,8 +32,13 @@ $(document).ready(function(){
                             expDate,
                         },
                         success: function() {
+                            event.preventDefault();
+                            $('#product-name').val('');
+                            $('#product-quantity').val('');
+                            $('#product-price').val('');
+                            $('#ExpDate').val('');
                             alert('product successfully added');
-                            return;//
+                            // return;
                         }
                     })
                 }
@@ -51,23 +56,25 @@ $(document).ready(function(){
     //read all products function
     $.getJSON("http://localhost:3000/products", function(data){
         let products_details = '';
+        let totalNum = 1;
         $.each(data, function(key,value){
-           
             products_details += `<tr id=${value.id}>
-            <td>${value.id}</td>
+            <td>${totalNum+key}</td>
              <td> ${value.productName}</td>
             <td>${value.productQuantity}</td>
             <td><span>&#8358;</span>${value.productPrice}</td>
             <td>${value.expDate}</td>
+             <td><a href='update.html?id=${value.id}'><button type="button" class="editbtn btn btn-primary" >Edit</button></a></td>
              <td><button type="button" class="delbtn btn btn-danger"  data-removeid="${value.id}">Delete</button></td>
            </tr>`
         });
         $('#products_table').append(products_details);
+        //delete function
         $('.delbtn').on('click',function(event){
             event.preventDefault();
-            console.log(event)
+            // console.log(event)
             const id = event.currentTarget.dataset.removeid;
-            console.log(id);
+            // console.log(id);
             $.ajax({
                 method: "DELETE",
                 url: `http://localhost:3000/products/${id}`,
@@ -77,15 +84,46 @@ $(document).ready(function(){
                 }
             });
         });
+        //save functionality
+        $('#Save').click(function(){
+            const getId = new URLSearchParams(window.location.search)
+            const idedit = getId.get('id')
+            const productName = $('#product-name').val();
+            const productQuantity = $('#product-quantity').val();
+            const productPrice = $('#product-price').val();
+            const expDate = $('#ExpDate').val();
+            console.log(idedit);
+            $.ajax({
+                method: 'PATCH',
+                url: `http://localhost:3000/products/${idedit}`,
+                data: {
+                    // productName,
+                    productQuantity,
+                    productPrice,
+                    expDate,
+                },
+                success: function() {
+                    event.preventDefault();
+                    // $('#product-name').val('');
+                    $('#product-quantity').val('');
+                    $('#product-price').val('');
+                    $('#ExpDate').val('');
+                    alert('product successfully added');
+                    // return;
+                }
+            })
+        })
+            // $.ajax({
+            //     method: "PATCH",
+            //     url: `http://localhost:3000/products/${id}`,
+            //     success: function(){
+            //         // $(`#${}`)
+            //     }
+            // })
+       
     });
-    //delete function
     
-    // $('#delete').click(function(event){
-    //     event.preventDefault();
-    //     $.ajax({
-    //         url:
-    //     })
-    // })
+    
 
 
 
